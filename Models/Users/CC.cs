13917bcca69a842fd8966c7a5a88f45a448d4fea
MyzusSteve <minhhieusw2279@gmail.com>
2018-11-17 17:12:57 +0700

    cc 2

---
+++ Models/Users/CC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcMnt.Models.Users
+{
+    public class CC
+    {
+        public CC()
+        {
+            int a = 1;
+            Console.WriteLine(a);
+        }
+    }
+}
